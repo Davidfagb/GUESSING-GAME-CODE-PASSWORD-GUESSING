67ad93bc0ce64ac33845e2d6435d3204d3d0f36d
DAVID FAGBUYIRO <fagbuyirodavid@yahoo.com>
2024-06-20 10:09:30 +0100

    Add files via upload

---
+++ guessing.py
@@ -0,0 +1,63 @@
+'''
+
+#GUESSING GAME CODE PASSWORD GUESSING
+
+import random
+player_name = input('Enter your name')
+password = input('Enter your password')
+password1 = input('Enter your password')
+password2 = input('Enter your password')
+for password in x:
+    if password in x:
+        print(name, 'Welcome you have successfully sign in')
+    else:
+        print ("invalid password")
+        
+        
+'''
+"""password = ('jentle')
+passwords = ('emmanuel')
+passwordss = ('precious')
+
+username = input('Enter your username')
+userInput = input('Enter your password')
+
+if userInput == (password):
+    print(welcome)
+
+elif userInput == (passwords):
+    print(welcome)
+
+elif userInput == (passwordss):
+    print(welcome)
+    
+
+else:
+        print('You entered a wrong password')"""
+
+
+print("Welcome \n please Login ")
+username = str(input("Enter Your Username: "))   
+first_pass = str(input("Enter Your Password: "))
+confirm_pass = ("Password1234")
+guess_count = 0
+limit = 3
+#while guess_count < limit :
+    #guess_count += 1
+if first_pass == confirm_pass:
+    print("Your Password IS Correct")
+else:
+    print("Invalid Password")
+
+    
+    
+        
+                      
+        
+
+
+    
+            
+
+
+                    
